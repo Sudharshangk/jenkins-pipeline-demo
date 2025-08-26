60c61477b65c52acb96e01c863f74d21ed7c020c
unknown <sudharshangk7702@gmail.com>
2025-08-26 12:43:54 +0530

    Version Update5

--- src/test/java/com/example/ci/AppTest.java
+++ src/test/java/com/example/ci/AppTest.java
@@ -4,9 +4,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
+
     @Test
     void addsNumbers() {
         App app = new App();
         assertEquals(5, app.add(2, 3));
     }
+
+    @Test
+    void addsWithZero() {
+        App app = new App();
+        assertEquals(7, app.add(7, 0));  
+        assertEquals(0, app.add(0, 0));   
+    }
+
+    @Test
+    void addsNegativeNumbers() {
+        App app = new App();
+        assertEquals(-5, app.add(-2, -3)); 
+        assertEquals(1, app.add(4, -3));   
+    }
+
 }
